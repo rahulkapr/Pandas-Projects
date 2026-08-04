9c239838d5715bd696f477e0d5236a8c4c8ac1b0
studentcode <rahulkapar1122@gmail.com>
2026-08-04 12:08:13 +0530

    Update sleep and doomscrolling analysis

--- Sleep & Doomscrolling Analysis/main.py
+++ Sleep & Doomscrolling Analysis/main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import os
-
 # Get the folder where main.py is located
 current_folder = os.path.dirname(os.path.abspath(__file__))
 
@@ -10,11 +9,14 @@ csv_file = os.path.join(current_folder, "sleep_doomscrolling_habits.csv")
 # Read the CSV file
 df = pd.read_csv(csv_file)
 
-# Display the first 5 rows
+print("..............BEGINNER LEVEL...............")
+
+
+#Display the first 5 rows.
 print("Display the first 5 rows:")
 print(df.head())
 
-# Display the last 5 rows
+#Display the last 5 rows.
 print("\nDisplay the last 5 rows:")
 print(df.tail())
 
@@ -22,5 +24,40 @@ print(df.tail())
 rows, columns = df.shape
 
 print("\nShape of the dataset:")
+
+#Number of rows
 print("Number of rows:", rows)
+
+#Number of columns
 print("Number of columns:", columns)
+
+#Display all column names.
+print("ALL Clumns Names")
+for column in df.columns:
+    print(column)
+
+
+#Check the data types of every column.
+print("Data types of every column")
+for column in df.columns:
+    print(column, ":", df[column].dtypes)
+
+
+#Display dataset information.
+print("Dataset Information")
+print(df.info())    
+
+#Generate descriptive statistics.
+print("Descriptive Statistics")
+print(df.describe())
+
+
+#Check for missing values.
+print("Missing Values")
+print(df.isnull().sum())
+
+
+#Count missing values in each column.
+print("Count missing values in each column")
+for column in df.columns:
+    print(column, ":", df[column].isnull().sum())
